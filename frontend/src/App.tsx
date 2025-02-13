d550d9513e8fc932a4af625088fea50b8d2e931e
timoteostifft <timoteostifft@gmail.com>
2025-02-13 00:04:35 -0300

    feat: first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-4xl font-bold text-blue-600 hover:text-blue-800">
+        Hello World
+      </h1>
+    </>
+  );
+}
+
+export default App;
