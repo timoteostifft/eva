7dc826b03237f5e835cc02529bc6c6691a70db33
timoteostifft <timoteostifft@gmail.com>
2025-02-14 00:44:26 -0300

    feat: paginate

---
+++ frontend/src/components/Paginate.tsx
@@ -0,0 +1,31 @@
+import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/outline";
+
+interface PaginateProps {
+  current: number;
+  onChange: (page: number) => void;
+}
+
+export const Paginate = ({ current, onChange }: PaginateProps) => {
+  return (
+    <div className="flex items-center gap-4 justify-center">
+      <button
+        onClick={() => onChange(current - 1)}
+        className="p-2 rounded hover:bg-gray-100 disabled:opacity-50 disabled:hover:bg-transparent"
+        aria-label="Página anterior"
+        disabled={current <= 1}
+      >
+        <ChevronLeftIcon className="h-6 w-6" />
+      </button>
+
+      <span className="font-medium">{current}</span>
+
+      <button
+        onClick={() => onChange(current + 1)}
+        className="p-2 rounded hover:bg-gray-100"
+        aria-label="Próxima página"
+      >
+        <ChevronRightIcon className="h-6 w-6" />
+      </button>
+    </div>
+  );
+};
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -4,26 +4,28 @@ import { useApi } from "../hooks/useApi";
 import { User } from "../types/user";
 import { useEffect, useState } from "react";
 import { useDebounce } from "../hooks/useDebounce";
-
+import { Paginate } from "../components/Paginate";
 export default function Home() {
   const [search, setSearch] = useState("");
+  const [page, setPage] = useState(1);
 
   const debounced = useDebounce(search, 500);
 
   const { data, loading, fetch } = useApi<User[]>({
     method: "GET",
-    endpoint: `/users?page=${1}${search ? `&name=${search}` : ""}`,
+    endpoint: `/users?page=${page}${search ? `&name=${search}` : ""}`,
   });
 
   useEffect(() => {
     fetch();
-  }, [debounced]);
+  }, [debounced, page]);
 
   return (
     <div className="px-20 pt-10 flex flex-col min-h-screen max-w-[800px] mx-auto gap-y-4">
       <h2 className="text-2xl font-bold text-gray-800">Usuários</h2>
       <Search value={search} onChange={setSearch} />
       <UsersList users={data} loading={loading} />
+      <Paginate current={page} onChange={setPage} />
     </div>
   );
 }
